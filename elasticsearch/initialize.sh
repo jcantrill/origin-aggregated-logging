49d05855c0b32e3b3eaf93c693928325a696341e
Jeff Cantrill <jcantril@redhat.com>
2015-09-15 14:35:36 -0400

    updated bin location

--- elasticsearch/initialize.sh
+++ elasticsearch/initialize.sh
@@ -3,5 +3,5 @@
 nohup /usr/share/elasticsearch/bin/elasticsearch -Des.pidfile=/var/run/elasticsearch/elasticsearch.pid &
 sleep 15; /usr/share/elasticsearch/bin/plugin -i io.fabric8/elasticsearch-cloud-kubernetes/1.2.1
 sleep 15; /usr/share/elasticsearch/bin/plugin -i com.floragunn/search-guard/0.5
-sleep 15; /usr/share/elasticsearch/bin/plugin -i file:////${AOP_ES_POLICY_SYNC_PLUGIN:?"The path to the ES SearchGuard policy sync is not set"}
+sleep 15; /usr/share/elasticsearch/bin/plugin -i openshift-searchguard-sync -u http://file.rdu.redhat.com/jcantril/openshift-searchguard-sync-0.1-SNAPSHOT.zip
 sleep 30; kill `cat /var/run/elasticsearch/elasticsearch.pid`
