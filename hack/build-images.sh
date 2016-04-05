aed372c285aaa7441ae6cde63568bc70234a13cb
Jeff Cantrill <jcantril@redhat.com>
2016-04-05 15:26:28 -0400

    DELETE ME

--- hack/build-images.sh
+++ hack/build-images.sh
@@ -2,14 +2,33 @@
 
 set -ex
 
-prefix=${PREFIX:-${1:-openshift/origin-}}
-version=${VERSION:-${2:-latest}}
-docker build -t "${prefix}logging-fluentd:${version}"       ../fluentd/
-docker build -t "${prefix}logging-elasticsearch:${version}" ../elasticsearch/
-docker build -t "${prefix}logging-kibana:${version}"        ../kibana/
-docker build -t "${prefix}logging-deployment:${version}"    ../deployment/
+prefix="openshift/origin-"
+version="latest"
+push=false
 
-if [ -n ${PUSH:-$3} ]; then
+source_root=$(dirname "${BASH_SOURCE}")/..
+
+for args in "$@"
+do
+  case $args in
+    --prefix=*)
+      prefix="${args#*=}"
+      ;;
+    --version=*)
+      version="${args#*=}"
+      ;;
+    --push)
+      push=true
+    ;;
+  esac
+done
+
+docker build -t "${prefix}logging-fluentd:${version}"       ${source_root}/fluentd/
+docker build -t "${prefix}logging-elasticsearch:${version}" ${source_root}/elasticsearch/
+docker build -t "${prefix}logging-kibana:${version}"        ${source_root}/kibana/
+docker build -t "${prefix}logging-deployment:${version}"    ${source_root}/deployment/
+
+if [ "$push" = true ]; then
 	docker push "${prefix}logging-fluentd:${version}"
 	docker push "${prefix}logging-elasticsearch:${version}"
 	docker push "${prefix}logging-kibana:${version}"
